15bdfdd15b9636c5e65578b5d15d72b02d12ae5a
Harsh Dave <hdave009@gmail.com>
2021-09-18 04:09:00 -0400

    Adding to grocery list functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
+var HOUSE
+var USER
+
 $(document).ready(() => {
-  getGroceryData('', createGroceryCards)
-  $("#grocery_search_bar").on('input', function(){
-    var query = $("#grocery_search_bar").val()
-    getGroceryData(query, createGroceryCards)
+  fetch("../json/users.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    USER = data[localStorage.getItem('user')]
+    fetch("../json/houses.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      HOUSE = data[USER['house_id']]
+      console.log(USER, HOUSE)
+      $('#house_header').text(HOUSE['address'])
+      getGroceryData('Apple', createGroceryCards)
+      $("#grocery_search_bar").on('input', function(){
+        var query = $("#grocery_search_bar").val()
+        getGroceryData(query, createGroceryCards)
+      });
+    })
   });
 })
 
-function getGroceryData(queryString="", callback) {
+function getGroceryData(queryString="Apple", callback) {
   var endpoint = `https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=${queryString}`
   $.ajax({
     url : endpoint,
@@ -29,14 +48,12 @@ function createGroceryCards (groceries) {
     cardString += `
 
     <div id='food_card_${food_info['foodId']}' data-foodId=${food_info['foodId']} class="card" style="width: auto; margin: 2.5%">
-    <img src="${food_info['image'] === null ? 'https://p.kindpng.com/picc/s/79-798754_hoteles-y-centros-vacacionales-dish-placeholder-hd-png.png' : food_info['image']}" alt="${food_info['label']}" width='50px' height='50px'/>
+    <img src="${food_info['image'] === null ? 'https://p.kindpng.com/picc/s/79-798754_hoteles-y-centros-vacacionales-dish-placeholder-hd-png.png' : food_info['image']}" alt="${food_info['label']}" width='100px' height='100px'/>
     <div class="card-body">
       <h5 class="card-title">${food_info['label']}</h5>
       <i class="card-text">${food_info['category']}
       </i>
-      <a href="#" class="btn btn-primary purple">Accept</a>
-      <a href="#" class="btn btn-secondary">
-        <i class="feather-sm" data-feather="maximize-2"></i>
+      <a id="add_food_btn_${food_info['foodId']}" data-category='${food_info['category']}' data-foodId='${food_info['foodId']}' data-label='${food_info['label']}' data-image='${food_info['image']}' class="btn btn-secondary">+
       </a>
     </div>
   </div>
@@ -52,9 +69,7 @@ function createGroceryCards (groceries) {
       <h5 class="card-title">${food_info['label']}</h5>
       <i class="card-text">${food_info['category']}
       </i>
-      <a href="#" class="btn btn-primary purple">Accept</a>
-      <a href="#" class="btn btn-secondary">
-        <i class="feather-sm" data-feather="maximize-2"></i>
+      <a id="add_food_btn_${food_info['foodId']}" data-category='${food_info['category']}' data-foodId='${food_info['foodId']}' data-label='${food_info['label']}' data-image='${food_info['image']}' class="btn btn-secondary">+
       </a>
     </div>
   </div>
@@ -62,7 +77,41 @@ function createGroceryCards (groceries) {
   }
 
   $('#groceries-container').empty()
+  $("[id^='add_food_btn_']").off('click')
   $('#groceries-container').append(cardString)
+  $("[id^='add_food_btn_']").on('click', (e) => {
+    let data = e.target.dataset
+    HOUSE['grocery_list'][data.foodid]= {foodId: data.foodid, category: data.category, label: data.label, image: data.image}
+    console.log(HOUSE)
+    createAddedGroceryCards()
+  })
+}
+
+function createAddedGroceryCards () {
+  cardString = ``
+  for(let food_info of Object.values(HOUSE['grocery_list'])) {
+    cardString += `
+
+    <div id='added_card_${food_info['foodId']}' data-foodId=${food_info['foodId']} class="card" style="width: auto; margin: 2.5%">
+    <img src="${food_info['image'] === null ? 'https://p.kindpng.com/picc/s/79-798754_hoteles-y-centros-vacacionales-dish-placeholder-hd-png.png' : food_info['image']}" alt="${food_info['label']}" width='100px' height='100px'/>
+    <div class="card-body">
+      <h5 class="card-title">${food_info['label']}</h5>
+      <i class="card-text">${food_info['category']}
+      </i>
+      <a id="remove_food_btn_${food_info['foodId']}" data-foodId='${food_info['foodId']}' class="btn btn-secondary">-
+      </a>
+    </div>
+  </div>
+    `
+  }
+  $('#added-groc-container').empty()
+  $("[id^='remove_food_btn_']").off('click')
+  $('#added-groc-container').append(cardString)
+  $("[id^='remove_food_btn_']").on('click', (e) => {
+    delete HOUSE['grocery_list'][e.target.dataset.foodid]
+    console.log(HOUSE)
+    createAddedGroceryCards()
+  })
 }
 
 
